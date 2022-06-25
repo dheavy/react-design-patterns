2c51a21240b29f0a9f126f81c9603f76b22354df
Davy Peter BRAUN <hello@davybraun.com>
2022-06-25 13:08:25 +0200

    feat: create resource loader component as container

--- container-components/src/App.js
+++ container-components/src/App.js
@@ -1,11 +1,23 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
+import { UserLoader } from './UserLoader';
+import { ResourceLoader } from './ResourceLoader';
 import { UserInfo } from './UserInfo';
+import { ProductInfo } from './ProductInfo';
 
 function App() {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
+    <>
+      <UserLoader userId="3456">
+        <UserInfo />
+      </UserLoader>
+
+      <ResourceLoader resourceUrl="/users/2345" resourceName="user">
+        <UserInfo />
+      </ResourceLoader>
+
+      <ResourceLoader resourceUrl="/products/2345" resourceName="product">
+        <ProductInfo />
+      </ResourceLoader>
+    </>
   );
 }
 
